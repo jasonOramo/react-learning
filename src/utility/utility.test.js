750cc8bbf1322c2729905d13ec2b61ab2ddab2fc
jasonOramo <3431636500@qq.com>
2019-09-08 10:33:54 +0800

    add unit tests for getValidateNum
    add unit tests for getValidateNum func

--- src/utility/utility.test.js
+++ src/utility/utility.test.js
@@ -8,5 +8,12 @@ describe('normal value',()=>{
 	});
 	it('string input for the curre',()=>{
 		expect(getValidateNum('sf')).toBe(0);
+	});
+	it('check rules',()=>{
+		expect(getValidateNum(10,100,{max:99,min:10})).toBe(99);
+		expect(getValidateNum(0,2,{max:100, min:10})).toBe(10);
+	});
+	it('check neggative value',()=>{
+		expect(getValidateNum(-10,20)).toBe(10);
 	})
 })
