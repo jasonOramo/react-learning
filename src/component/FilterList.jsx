1743a8c5a56a2a9e735ea611aaba7e2d4572d3e4
jasonOramo <3431636500@qq.com>
2019-09-11 23:42:43 +0800

    filter list
    filter list

---
+++ src/component/FilterList.jsx
@@ -0,0 +1,32 @@
+import React,{Component} from 'react';
+
+class FilterList extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			filter : props.filter || ''
+		};
+		this.handleChange = this.handleChange.bind(this);
+	}
+	handleChange(event){
+		this.setState({'filter': event.target.value});
+	}
+	_getItems(items,filter){
+		const res = items.filter((item)=>{return item.name.indexOf(filter) > -1 ? true : false;});
+		return res;
+	}
+	render(){
+		const {items} = this.props;
+		const filterdItems = this._getItems(items,this.state.filter);
+		return (
+			<>
+			<input value = {this.state.filter} onChange={this.handleChange} />
+			<ul>
+				{filterdItems.map(item=><li key={item.key}>{item.name}</li>)}
+			</ul>
+			</>
+			);
+	}
+}
+
+export default FilterList;
---
+++ src/component/FilterList.test.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from "react-dom";
+import { act, Simulate } from "react-dom/test-utils";
+
+
+import FilterList from './FilterList';
+
+var container = null;
+
+beforeEach(()=>{
+	container = document.createElement('div');
+	document.body.appendChild(container);
+});
+
+afterEach(()=>{
+	unmountComponentAtNode(container);
+	container.remove();
+	container = null;
+});
+
+
+describe('normal test',()=>{
+	let items = [];
+	for(let i = 0; i < 10; i++){
+		items.push({
+			key: i,
+			name:'items'+i
+		});
+	}
+
+	it('normal render',()=>{
+		act(()=>{
+			render(<FilterList items = {items} filter = {''} />,container);
+		})
+		const filterList = document.querySelector('ul');
+		expect(filterList).not.toBeNull();
+	});
+	it('change input',()=>{
+		act(()=>{
+			render(<FilterList items = {items} filter = {''} />,container);
+		})
+
+		const filterInput = document.querySelector('input');
+		Simulate.change(filterInput,{'target':{'value':20}});
+		const filterItems = document.querySelector('li');
+		expect(filterItems).toBeNull();
+	})
+})
